71ac234f280e8a13fb807886b5d026d50b5765fc
Iancovski <46848500+Iancovski@users.noreply.github.com>
2024-05-11 00:20:11 -0300

    Updating dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default [
+    {
+        plugins: {
+            '@stylistic': stylistic
+        },
+        rules: {
+            '@stylistic/indent': ['error', 4],
+            '@stylistic/quotes': ['error', 'single'],
+            '@stylistic/semi': ['error', 'always'],
+        },
+        languageOptions: {
+            globals: globals.node
+        }
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+];
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
-import cors from 'cors';
 import express from 'express';
 import userRouter from './routes/user.route';
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRouter);
